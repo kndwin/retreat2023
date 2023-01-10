b73117b42acf7acb4b592cc32f090bee8fdd36ac
Kevin Nguyen <me@kndwin.dev>
2023-01-10 19:47:10 +1100

    ➕: added correct github link

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,12 +1,14 @@
-
 const theme = {
   logo: <span>Kids Retreat 2023</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/kndwin/retreat2023",
   },
   // https://github.com/shuding/nextra/pull/1206
   toc: {
-		extraContent: () => <></>,
+    extraContent: () => <></>,
+  },
+  footer: {
+		component: () => <></>
   },
 };
 
