48cdc9c26b665eeb1c199037533d622a0b111b76
Kevin Nguyen <me@kndwin.dev>
2023-01-10 19:29:10 +1100

    🚀 first init

---
+++ theme.config.jsx
@@ -0,0 +1,13 @@
+
+const theme = {
+  logo: <span>Kids Retreat 2023</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // https://github.com/shuding/nextra/pull/1206
+  toc: {
+		extraContent: () => <></>,
+  },
+};
+
+export default theme;
