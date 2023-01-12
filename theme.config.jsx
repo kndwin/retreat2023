638f424b5c0ae246528d9d4e7130106974f06e5d
Kevin Nguyen <me@kndwin.dev>
2023-01-12 12:49:29 +1100

    ➕: added SEO head

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,12 @@
 const theme = {
   logo: <span>Kids Retreat 2023</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Kids Retreat 2023" />
+      <meta property="og:description" content="St John's Kids retreat 2023" />
+    </>
+  ),
   useNextSeoProps: () => {
     return {
       titleTemplate: "%s",
